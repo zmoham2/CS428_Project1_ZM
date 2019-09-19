95b1314ac7b4841bc7e56ae5c6cc6b2bad1c46ee
zmoham2 <zmoham2@uic.edu>
2019-09-18 22:17:36 -0500

    changed made

---
+++ ClashofKingsFinal/Assets/ambientscript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ambientscript : MonoBehaviour
+{
+
+    public GameObject thing, MyObject;
+    AudioSource audioData;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //MySource.Stop();
+        audioData = GetComponent<AudioSource>();
+
+        thing.GetComponent<GameObject>();
+        if (MyObject.activeSelf == true)
+            audioData.Play();
+        else
+            audioData.Stop();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
