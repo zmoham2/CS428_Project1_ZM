c595fe0b1428d07b0c5c2f42ffc58d4d26ff5d4d
zmoham2 <zmoham2@uic.edu>
2019-09-19 17:53:17 -0500

    final files added

--- ClashofKingsFinal/Assets/ambientscript.cs
+++ ClashofKingsFinal/Assets/ambientscript.cs
@@ -3,32 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
 
-public class ambientscript : MonoBehaviour
+// THIS CODE WAS SOURCED FROM https://developer.vuforia.com/forum/faq/unity-how-can-i-play-audio-when-targets-get-detected
+// IT'S STRAIGHT FROM VUFORIA! Needed this to ensure the ambient noise plays on detection since we're unable to edit the built-in scripts
+
+public class ambientscript : MonoBehaviour,ITrackableEventHandler
 {
+    private TrackableBehaviour mTrackableBehaviour;
+    public AudioSource audio;
 
-    public GameObject thing, MyObject;
-    AudioSource audioData;
-
-
-    // Start is called before the first frame update
     void Start()
     {
-        //MySource.Stop();
-        audioData = GetComponent<AudioSource>();
-
-        thing.GetComponent<GameObject>();
-        if (MyObject.activeSelf == true)
-            audioData.Play();
-        else
-            audioData.Stop();
-
-
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
     {
-        
-
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            // Play audio when target is found
+            audio.Play();
+        }
+        else
+        {
+            // Stop audio when target is lost
+            audio.Stop();
+        }
     }
 }
